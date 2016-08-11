3c4dc0aefe5b1563e1ddad8142de170283ae634c
Bucky <buckyroberts@gmail.com>
2016-08-10 17:23:54 -0700

    First push

--- buckets.py
+++ buckets.py
@@ -3,14 +3,14 @@ import boto3
 s3 = boto3.resource('s3')
 
 # creating a bucket
-# s3.create_bucket(Bucket='uniqueBucketName')
+s3.create_bucket(Bucket='uniqueBucketName')
 
 # print out all bucket names
 for bucket in s3.buckets.all():
     print(bucket.name)
 
 # storing data
-# s3.Object('uniqueBucketName', 'sample.txt').put(Body=open('sample.txt', 'rb'))
+s3.Object('uniqueBucketName', 'sample.txt').put(Body=open('sample.txt', 'rb'))
 
 for bucket in s3.buckets.all():
     for key in bucket.objects.all():
